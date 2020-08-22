017aa935d61860d3e7e68398007dd253f2836b60
Joseph Baca <josephcbaca@gmail.com>
2020-08-21 20:51:49 -0700

    Updated App.js, Navbar and Home buttons without functionality.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,25 +1,11 @@
 import React from "react";
 
-function Navbar ()
-{
-    return(
-        <div className="container">
-        <ul class="navbar-nav navbar-expand-lg navbar-light bg-light">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link startGameButton" href="./StartGame.js">Start Game</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link loginButton" href="./Login.js">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link signUpButton" href="./SignUp.js">Sign Up</a>
-          </li>
-        </ul>
-      </div>
-    );
+function Navbar() {
+  return (
+    <div>
+      <h3>Golfclub</h3>
+    </div>
+  );
 }
 
 export default Navbar;
