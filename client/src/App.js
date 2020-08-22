148305c2efbfd6dff747801e8ddaf52d92020d62
Joseph Baca <josephcbaca@gmail.com>
2020-08-21 22:39:15 -0700

    Adding React Router starting with Create Game and Login from Home.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/pages/Home"
+import { Route } from 'react-router-dom';
+import Login from "./components/pages/Login";
+import CreateGame from "./components/pages/CreateGame";
 
-const App = () => 
-<div>
-<Home />
-</div>
-
+const App = () =>
+  <div>
+    <Route extact path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/create-game" component={CreateGame} />
+  </div>
 export default App;
 
