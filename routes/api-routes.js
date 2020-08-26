993b262e8ddd9bdbf499fedc97b19d971d413c24
nobobobo <ianhsu1221@gmail.com>
2020-08-25 18:57:44 -0700

    POST, PUT route for creating, joining a game

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,35 +1,66 @@
 const db = require("../models");
 
-module.exports = function(app){
-  
-  app.get("/api/test", function(req, res) {
-    res.json({Result: "OK! React can connect to the backend server."});
+module.exports = function (app) {
+
+  app.get("/api/test", function (req, res) {
+    res.json({ Result: "OK! React can connect to the backend server." });
   });
 
-  app.get("/api/games", function(req, res) {
+  app.get("/api/games", function (req, res) {
     db.Game.findAll({
       include: [{
-          as: 'host',
-          model: db.Player,
-          attributes: ['id', 'userName']
-        },{
-          as: 'player_1',
-          model: db.Player,
-          attributes: ['id', 'userName']
-        },{
-          as: 'player_2',
-          model: db.Player,
-          attributes: ['id', 'userName']
-        },{
-          as: 'player_3',
-          model: db.Player,
-          attributes: ['id', 'userName']
-        }
+        as: 'host',
+        model: db.Player,
+        attributes: ['id', 'userName']
+      }, {
+        as: 'player_1',
+        model: db.Player,
+        attributes: ['id', 'userName']
+      }, {
+        as: 'player_2',
+        model: db.Player,
+        attributes: ['id', 'userName']
+      }, {
+        as: 'player_3',
+        model: db.Player,
+        attributes: ['id', 'userName']
+      }
       ],
-      attributes: ['id', 'gameTime','golfCourse']
-    }).then(games => res.json(games)); 
+      attributes: ['id', 'gameTime', 'golfCourse']
+    }).then(games => res.json(games));
   });
 
+  app.post("/api/newgame", function (req, res) {
+    if (!req.user) {
+      res.json({ msg: "Not logged in!" });
+    } else {
+      db.Game.create({
+        gameTime: req.body.gameTime,
+        host_id: req.user.id,
+        golfCourse: req.body.golfCourse
+      }).then(newGame => res.json(newGame));
+    }
+  });
+
+  app.put("api/joingame", function (req, res) {
+    // for actuall put request, req.body needs: 
+    // id(game id), user_id, slot_number (the id of empty player slot, e.g. player_id_2, player_id_3)
+
+    if (!req.user) {
+      res.json({ msg: "Not logged in!" });
+    } else {
+      // e.g if req.body.slot_number is "player_id_2"
+      // pubBody will be {player_id_2: user_id}
+      const putBody = {};
+      putBody[req.body.slot_number] = req.user.id;
+
+      db.Game.update(pubBody, {
+        where: {
+          id: req.body.id
+        }
+      }).then(result => res.json(result))
+    }
+  });
 }
 
 //Sign Up Create User POST
@@ -37,7 +68,7 @@ module.exports = function(app){
 
 //Select User GET
 
-//Create Game POST
-//Will need to GET from current logged in session
+//Create Game POST √
+//Will need to GET from current logged in session 
 
-//Join Game PUT
+//Join Game PUT √
