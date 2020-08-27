50050d744986c7d4f664629154d6b12b849b6306
Joseph Baca <josephcbaca@gmail.com>
2020-08-26 21:14:06 -0700

    Added login functionality without authentication.

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -3,21 +3,48 @@ import { Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  function loginUser () {
+    const bodyObj = {  
+      email: email,
+      password: password
+    }
+    console.log(bodyObj);
+    fetch("/api/login", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(bodyObj)
+    }).then(res=> window.location.replace("/browse-game"));
+  }
+
   return (
     <div>
       {<Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control className="col-4" type="email" placeholder="Enter email" />
+          <Form.Control onChange={e => setEmail(e.target.value)} 
+          className="col-4" 
+          type="email" 
+          placeholder="Enter email" />
           <Form.Text className="text-muted">
             Please enter your login email.
     </Form.Text>
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control className="col-4" type="password" placeholder="Password" />
+          <Form.Control onChange={e => setPassword(e.target.value)} 
+          className="col-4" 
+          type="password" 
+          placeholder="Password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button onClick={loginUser}
+        variant="primary" 
+        type="submit">
           Submit
         </Button>
       </Form>}
