073aa270b50bb9ee52cc83923c9bb21d5037d6d3
nobobobo <ianhsu1221@gmail.com>
2020-09-02 20:42:05 -0700

    NavBar with logout

--- client/src/components/pages/BrowseGame/index.js
+++ client/src/components/pages/BrowseGame/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import GameContainer from '../../GameContainer';
 import { Link } from 'react-router-dom';
+import Navbar from "../../navbar"
 
 function BrowseGame({loggedIn, currentUser}) {
+    
     if (!loggedIn) return (<div> Please log in before browsing games! <Link to="/login">Login</Link>
     </div>);
     return (<div>
+                <Navbar loggedIn={loggedIn}/>
                 <button><Link to="/create-game">Create Game</Link></button>
                 <p> Join a hosted game: </p>
                 <GameContainer currentUser={currentUser} />
