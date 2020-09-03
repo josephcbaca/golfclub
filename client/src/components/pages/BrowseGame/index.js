5d3f9ff138017ec748a8acb61715dbeb9e12dfe7
Joseph Baca <josephcbaca@gmail.com>
2020-09-02 20:56:45 -0700

    Updated style buttons on Browse Game page.

--- client/src/components/pages/BrowseGame/index.js
+++ client/src/components/pages/BrowseGame/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import GameContainer from '../../GameContainer';
 import { Link } from 'react-router-dom';
 import Navbar from "../../navbar"
+import './style.css'
 
 function BrowseGame({loggedIn, currentUser}) {
     
@@ -10,6 +11,7 @@ function BrowseGame({loggedIn, currentUser}) {
     return (<div>
                 <Navbar loggedIn={loggedIn}/>
                 <button><Link to="/create-game">Create Game</Link></button>
+                <Link className="create-game-button" to="/create-game"><button type="button" className="create-game-button btn btn-outline-success col-12">Create Game</button></Link>
                 <p> Join a hosted game: </p>
                 <GameContainer currentUser={currentUser} />
              </div> );
