07313fa33e130522f9e648a12bbc76f8e4163f2d
Joseph Baca <josephcbaca@gmail.com>
2020-09-02 21:05:10 -0700

    Updated button styling for Host button.

--- client/src/components/pages/BrowseGame/index.js
+++ client/src/components/pages/BrowseGame/index.js
@@ -4,17 +4,18 @@ import { Link } from 'react-router-dom';
 import Navbar from "../../navbar"
 import './style.css'
 
-function BrowseGame({loggedIn, currentUser}) {
-    
+function BrowseGame({ loggedIn, currentUser }) {
+
     if (!loggedIn) return (<div> Please log in before browsing games! <Link to="/login">Login</Link>
     </div>);
     return (<div>
-                <Navbar loggedIn={loggedIn}/>
-                <button><Link to="/create-game">Create Game</Link></button>
-                <Link className="create-game-button" to="/create-game"><button type="button" className="create-game-button btn btn-outline-success col-12">Create Game</button></Link>
-                <p> Join a hosted game: </p>
-                <GameContainer currentUser={currentUser} />
-             </div> );
+        <Navbar loggedIn={loggedIn} />
+        <div className="container">
+            <Link className="create-game-button" to="/create-game"><button type="button" className="create-game-button btn btn-outline-success col-2">Host Game</button></Link>
+            <p className="col-2"> Join a hosted game: </p>
+            <GameContainer currentUser={currentUser} />
+        </div>
+    </div>);
 }
 
 
