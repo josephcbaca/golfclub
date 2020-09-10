49abf0b691c928e8ae252fc3742fec577eca8405
nobobobo <ianhsu1221@gmail.com>
2020-09-09 23:46:06 -0700

    position

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
-import Mojs from "./mojs";
+import mojs from "@mojs/core";
 
 function Navbar({ loggedIn }) {
+
+  useEffect(()=>{
+    const animation = new mojs.Shape({
+      parent: "#mojs",
+      shape: "circle",
+      fill: { "#306754": "#5cb85c" },
+      radius: { 10: 15 },
+      y:1,
+      x:-1,
+      duration: 1500,
+      isYoyo: true,
+      isShowStart: true,
+      easing: "elastic.inout",
+      repeat: 999
+    });
+    animation.play();
+  },[]);
 
   function logout() {
     axios.get("/logout").then(window.location.replace("/"));
@@ -11,8 +28,8 @@ function Navbar({ loggedIn }) {
   return (
     <div className="container d-flex">
 
-      <h3 className="mr-auto nav-name">Golfclub</h3>
-      <Mojs />
+<span id="mojs"/>
+<h3 className="my-1 ml-4 mr-auto nav-name">Golfclub</h3>
       {loggedIn ? <button type="button" className="ml-auto btn btn-outline-success site-button" onClick={logout}> Logout </button> : null}
 
     </div>
